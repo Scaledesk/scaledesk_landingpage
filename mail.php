4cfc8382db12f4dba82bde78fd7effc0595ea491
niteshbca <singhniteshbca@gmail.com>
2016-03-02 20:17:17 +0530

    added mail message

--- mail.php
+++ mail.php
@@ -7,11 +7,11 @@ $email=$_POST['email'];
 
 $phone=$_POST['phone'];
 $name=$_POST['name'];
-
+$emailadmin="sanchit2411@gmail.com";
 $subject = "GET IN TOUCH.";
 $message ='<html>
 <body>
-<div id="abcd" style="text-align:justify;font-size:18px;"> Name:-'.$name.' </div>
+<div id="abcd" style="text-align:justify;font-size:18px;"> Name:-'.$name.'<br> Email:-'.$email.'<br>Phone:-'.$phone.'</div>
 
 
 </body>
@@ -19,7 +19,7 @@ $message ='<html>
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers  .= "From:sudo@scaledesk.com\r\n";
 $headers .= "Content-type: text/html;charset=iso-8859-1" . "\r\n";
-$retval = mail ($email,$subject,$message,$headers);
+$retval = mail ($emailadmin,$subject,$message,$headers);
  header("location: index.php");
 }
 else{
